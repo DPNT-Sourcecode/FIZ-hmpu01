207dae1d0bbddb934a23f67931317127b214aa93
sbx_user1060 <sbx_user1060@ip-10-128-163-141.eu-west-2.compute.internal>
2018-09-25 10:52:52 +0000

    Tue Sep 25 10:52:52 UTC 2018

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -3,9 +3,30 @@ package befaster.solutions.SUM;
 import befaster.runner.SolutionNotImplementedException;
 
 public class SumSolution {
+	
+	// Min value
+	private static final int MIN_BOUNDS = 0 ;
+	
+	// Max value
+	private static final int MAX_BOUNDS = 100 ;
+	
 
     public int compute(int x, int y) {
+    	int sum = 0 ;
+    	if(inBounds(x) && inBounds(y)) {
+    		
+    	}else {
+    		throw new InvalidSumException("Invalid input values x:"+x+" y:"+y);
+    	}
         throw new SolutionNotImplementedException();
     }
 
+    private boolean inBounds(int value) {
+    	boolean inBounds = false ;
+    	if(value >= MIN_BOUNDS && value <= MAX_BOUNDS) {
+    		inBounds = true ;
+    	}
+    	return inBounds;
+    }
+    
 }
