1b54774e31bd1775ed6d9c594d7409b26589cb54
sbx_user1059 <sbx_user1059@ip-10-128-196-180.eu-west-2.compute.internal>
2018-09-26 12:25:37 +0000

    Wed Sep 26 12:25:37 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -126,17 +126,20 @@ public class FizzBuzzSolution {
 		boolean div3 = false ;
 		boolean div5 = false ;
 		
-		if(getDivideByNumList(FIZZ).contains(number)) {
-			div3 = true ;
-			if(response.toString().indexOf("buzz")>-1 || response.toString().indexOf("buzz fake")>-1 || response.toString().indexOf("buzz deluxe")>-1) {
-				response.insert(0, "ossie fizz ");
-			}
-		}else if(getDivideByNumList(BUZZ).contains(number)) {
-			div5 = true ;
-			if(response.toString().indexOf("fizz") >-1 || response.toString().indexOf("fizz fake")>-1 || response.toString().indexOf("fizz deluxe")>-1) {
-				response.insert(4, " ossie buzz");
+		if(response.toString().indexOf("fizz buzz")>-1) {
+			if(number % FIZZ ==0) {
+				div3 = true ;
+				if(response.toString().indexOf("buzz")>-1 || response.toString().indexOf("buzz fake")>-1 || response.toString().indexOf("buzz deluxe")>-1) {
+					response.insert(0, "ossie fizz ");
+				}
+			}else if(number % BUZZ ==0) {
+				div5 = true ;
+				if(response.toString().indexOf("fizz") >-1 || response.toString().indexOf("fizz fake")>-1 || response.toString().indexOf("fizz deluxe")>-1) {
+					response.insert(4, " ossie buzz");
+				}
 			}
 		}
+		
 		
 		if(number.equals(546) || number.equals(30)) {
 			System.out.println(response.toString()+" div5"+div5+" div3"+div3);
